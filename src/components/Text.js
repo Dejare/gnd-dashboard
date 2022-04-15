d11047dc1361eec8e408179a90107372a02d7350
Adejare <inioluwaadesida@gmail.com>
2022-04-16 00:52:50 +0100

    first commit

---
+++ src/components/Text.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div className='mainText'>
+        <h1>Virtual Reality Makes <br/> Everything Look <span>Real</span></h1>
+        <p>Lorem ipsum dolor tjlnajnkavk jget new vhannels wutnfkf dknd lkdnlcnsdk <span>nldnldn lndklnldsnd lskdn lsd sl d sdlsndlksnldnlknl nkn snjnlrnnr rknrnnk rlnrnn;rkn irlrioonss</span></p>
+        <button>Buy Now</button>
+    </div>
+  )
+}
+
+export default Text
