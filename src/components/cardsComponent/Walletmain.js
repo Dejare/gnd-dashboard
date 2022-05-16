a91fdfc46922efec0a2fda8b31f5fd8379079e47
Adejare <inioluwaadesida@gmail.com>
2022-05-16 08:30:06 +0100

    fixed cors

--- src/components/cardsComponent/Walletmain.js
+++ src/components/cardsComponent/Walletmain.js
@@ -11,10 +11,10 @@ const apiKey = "bd9aa8a7e087001528a4349bbf18b357bca0e261";
 
     useEffect(() => {
         fetch( `https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&ids=BTC,ETH,LTC,DAI&interval=1d&convert=USD&per-page=100&page=1`, {headers: {
-            // 'Content-Type': 'application/json',
-            // "Access-Control-Allow-Headers": "X-Token,Origin,X-Requested-With,content-type" ,
-            // 'Access-Control-Allow-Methods': 'GET',
-            // 'Access-Control-Allow-Credentials': true         
+             'Content-Type': 'application/json',
+             "Access-Control-Allow-Headers": "X-Token,Origin,X-Requested-With,content-type" ,
+             'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Credentials': true         
         }})
         .then(res=> res.json())
         .then(answer=> {
