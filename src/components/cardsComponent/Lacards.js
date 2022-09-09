720babd62ff20f8db73fcc9ef27cc76493829c8d
dejare <inioluwaadesida@gmail.com>
2022-09-09 02:45:58 +0100

    fixed bug

--- src/components/cardsComponent/Lacards.js
+++ src/components/cardsComponent/Lacards.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import { useEffect } from 'react'
 import '../../App.scss'
 const Lacards = ({ imgSrc, laone, laonep, latwo, latwop, lathree, lathreep, lafour, lafourp, imgBg }) => {
-    const color = ["#f5cdcd", "#86dceb", "#ffc0f4", "aliceblue"]
-    useEffect(() => {
-
-
-        const iconColor = Math.floor(Math.random() * 4) + 1;
-
-        console.log(color[iconColor])
-
-    }, [iconColor])
-
-    const randomColor = color[iconColor]
 
     return (
         <div className="lacards">
-            <div className="laImg" style={{ backgroundColor: `${randomColor}` }}>
+            <div className="laImg" style={{ backgroundColor: `${imgBg}` }}>
                 <div>
                     {imgSrc}
                 </div>
